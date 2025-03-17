c17338e65dee0e7ab37b62407182b640d4399e84
emanuel0711 <emanuelchaves321@gmail.com>
2025-03-17 19:59:27 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/listadetarefas2/ListaDeTarefas2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.listadetarefas2;
+
+/**
+ *
+ * @author emanu
+ */
+public class ListaDeTarefas2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
