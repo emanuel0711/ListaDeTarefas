26d88d1a816fd60b38b98101c2c354ee57b09dc4
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-03-19 20:10:49 -0300

    Criando Usuario e UsuarioDAO

--- src/main/java/com/mycompany/listadetarefas2/model/Usuario.java
+++ src/main/java/com/mycompany/listadetarefas2/model/Usuario.java
@@ -2,12 +2,48 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.listadetarefas.model;
+package com.mycompany.listadetarefas2.model;
 
 /**
  *
  * @author emanu
  */
 public class Usuario {
-    
+	private int id;
+	private String nome;
+	private String password;
+
+	public Usuario(int id, String nome, String password) {
+		this.id = id;
+		this.nome = nome;
+		this.password = password;
+	}
+	public Usuario(String nome, String password) {
+		this.nome = nome;
+		this.password = password;	
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
--- src/main/java/com/mycompany/listadetarefas2/model/UsuarioDAO.java
+++ src/main/java/com/mycompany/listadetarefas2/model/UsuarioDAO.java
@@ -4,10 +4,57 @@
  */
 package com.mycompany.listadetarefas.model;
 
+import com.mycompany.listadetarefas2.model.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author emanu
  */
 public class UsuarioDAO {
-    
+    private Connection connection;
+
+
+
+public boolean createUsuario(String nome, String senha) throws Exception {
+        String sql = "INSERT INTO users (nome, senha) VALUES (?, ?)";
+
+        try ( PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, nome);
+            pstmt.setString(2, senha);
+
+            pstmt.executeUpdate();
+
+            System.out.println("Usuário Inserido com sucesso");
+        } catch (Exception e) {
+            throw new Exception("Erro ao inserir usuário" + e.getMessage());
+        }
+        return true;
+    }	
+    public Usuario getUsuario(String name) throws Exception {
+        String sql = "SELECT * FROM users WHERE name = ?";
+
+        Usuario user = null;
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, name);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) { // Verifica se há resultados antes de acessar os valores
+                user = new Usuario(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("hashedPassword")
+                );
+            } else {
+                throw new Exception("Usuário não foi encontrado");
+            }
+        } catch (Exception e) {
+            throw new Exception("Erro ao obter usuário " + name + ": " + e.getMessage());
+        }
+
+        return user;
+    }
 }
